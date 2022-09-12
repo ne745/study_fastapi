45860d5b4851597bb4257bf44dd4f2fbf6c572d8
ne745 <kkpma0@gmail.com>
2022-09-12 22:57:57 +0900

    PUT を追加

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 URL = 'http://localhost:8000/'
@@ -11,5 +12,15 @@ params = {
     'q': 'somequery'
 }
 res = requests.get(url_item, params=params)
+print(res.status_code)
+print(res.json())
+
+url_item = URL + 'items/100'
+data = {
+    'name': 'The item name',
+    'price': 0,
+    'is_offer': True
+}
+res = requests.put(url_item, json.dumps(data))
 print(res.status_code)
 print(res.json())
