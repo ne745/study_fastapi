4bdbb30b5f81b1664b95dc8b4c8839892a86c5f9
ne745 <kkpma0@gmail.com>
2022-09-14 22:42:13 +0900

    サーバ側のデータベース読み取り関数を実装

--- reservation_system/sql_app/server.py
+++ reservation_system/sql_app/server.py
@@ -1,15 +1,35 @@
-from fastapi import FastAPI
+from typing import List
 
-from sql_app.schemas import Booking, User, Room
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+import crud
+import models
+from database import SessionLocal, engine
+from schemas import Booking, User, Room
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get('/')
-async def index():
-    return {'message': 'Success'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
+# @app.get('/')
+# async def index():
+#     return {'message': 'Success'}
+
+
+##############################
+# CREATE #####################
+##############################
 @app.post('/bookings')
 async def bookings(bookings: Booking):
     return {'bookings': bookings}
@@ -23,3 +43,27 @@ async def users(users: User):
 @app.post('/rooms')
 async def rooms(rooms: Room):
     return {'rooms': rooms}
+
+
+##############################
+# READ #######################
+##############################
+@app.get('/bookings', response_model=List[Booking])
+async def read_bookings(
+        skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    bookings = crud.get_bookings(db, skip, limit)
+    return bookings
+
+
+@app.get('/users', response_model=List[User])
+async def read_users(
+        skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip, limit)
+    return users
+
+
+@app.get('/rooms', response_model=List[Room])
+async def read_rooms(
+        skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    rooms = crud.get_rooms(db, skip, limit)
+    return rooms
