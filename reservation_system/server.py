138b8693e5aac6763fc2c5e5ae5bd37aaad062ce
ne745 <kkpma0@gmail.com>
2022-09-14 21:55:22 +0900

    FastAPI のデータ構造を schemas.py に移動

--- reservation_system/server.py
+++ reservation_system/server.py
@@ -1,28 +1,6 @@
-import datetime
-
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
 
-
-class Booking(BaseModel):
-    booking_id: int
-    user_id: int
-    room_id: int
-    num_people: int
-    start_datetime: datetime.datetime
-    end_datetime: datetime.datetime
-
-
-class User(BaseModel):
-    user_id: int
-    user_name: str = Field(max_length=12)
-
-
-class Room(BaseModel):
-    room_id: int
-    room_name: str = Field(max_length=12)
-    capacity: int
-
+from sql_app.schemas import Booking, User, Room
 
 app = FastAPI()
 
