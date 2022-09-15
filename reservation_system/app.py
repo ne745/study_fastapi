27a538b5389cda6c6e1b97e59ab8e5c842810ad0
ne745 <kkpma0@gmail.com>
2022-09-15 21:53:50 +0900

    ユーザ登録画面をテスト画面から変更

--- reservation_system/app.py
+++ reservation_system/app.py
@@ -53,23 +53,25 @@ if page == 'booking':
 
 
 elif page == 'user':
-    st.title('API テスト画面 (ユーザ)')
+    st.title('ユーザ登録画面')
     with st.form(key=page):
-        user_id: int = random.randint(0, 10)
         user_name: str = st.text_input('ユーザ名', max_chars=12)
         data = {
-            'user_id': user_id,
             'user_name': user_name
         }
-        submit_button = st.form_submit_button(label='送信')
+        submit_button = st.form_submit_button(label='ユーザ登録')
 
     if submit_button:
         st.write('## 送信データ')
-        st.json(data)
 
         st.write('## レスポンス結果')
         res = requests.post(URL + '/users', json.dumps(data))
-        st.write(res.status_code)
+
+        if res.status_code == 200:
+            st.success('ユーザ登録完了')
+        else:
+            st.success('ユーザ登録失敗')
+            st.write(res.status_code)
         st.json(res.json())
 
 elif page == 'room':
