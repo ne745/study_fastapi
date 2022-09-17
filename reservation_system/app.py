1916e9bbe2d73a971357987cfadde9111f11444b
ne745 <kkpma0@gmail.com>
2022-09-17 21:43:51 +0900

    予約が 1 つも登録されていない場合に予約一覧を表示できないバグを修正

--- reservation_system/app.py
+++ reservation_system/app.py
@@ -49,47 +49,52 @@ if page == 'booking':
     else:
         st.write('予約可能な会議室はありません')
 
-    # 会議室一覧の取得
+    # 予約一覧の取得
     url_bookings = URL + '/bookings'
     res = requests.get(url_bookings)
     bookings = res.json()
     df_bookings = pd.DataFrame(bookings)
 
-    users_id = {}
-    for user in users:
-        users_id[user['user_id']] = user['user_name']
-
-    rooms_id = {}
-    for room in rooms:
-        rooms_id[room['room_id']] = {
-            'room_name': room['room_name'],
-            'capacity': room['capacity'],
-        }
-
-    # ID -> 値に変更
-    to_user_name = lambda x: users_id[x]  # noqa: E731
-    to_room_name = lambda x: rooms_id[x]['room_name']  # noqa: E731
-    to_datetime = lambda x: \
-        datetime.datetime.fromisoformat(x) \
-        .strftime('%Y/%m/%d %H:%M')  # noqa: E731
-
-    df_bookings['user_id'] = df_bookings['user_id'].map(to_user_name)
-    df_bookings['room_id'] = df_bookings['room_id'].map(to_room_name)
-    df_bookings['start_datetime'] = \
-        df_bookings['start_datetime'].map(to_datetime)
-    df_bookings['end_datetime'] = df_bookings['end_datetime'].map(to_datetime)
-
-    df_bookings = df_bookings.rename(columns={
-        'user_id': '予約者名',
-        'room_id': '会議室名',
-        'num_people': '予約人数',
-        'start_datetime': '開始時刻',
-        'end_datetime': '終了時刻',
-        'booking_id': '予約番号'
-    })
-
     st.write('### 予約一覧')
-    st.table(df_bookings)
+    if bookings:
+        # 予約があれば表示する
+        users_id = {}
+        for user in users:
+            users_id[user['user_id']] = user['user_name']
+
+        rooms_id = {}
+        for room in rooms:
+            rooms_id[room['room_id']] = {
+                'room_name': room['room_name'],
+                'capacity': room['capacity'],
+            }
+
+        # ID -> 値に変更
+        to_user_name = lambda x: users_id[x]  # noqa: E731
+        to_room_name = lambda x: rooms_id[x]['room_name']  # noqa: E731
+        to_datetime = lambda x: \
+            datetime.datetime.fromisoformat(x) \
+            .strftime('%Y/%m/%d %H:%M')  # noqa: E731
+
+        df_bookings['user_id'] = df_bookings['user_id'].map(to_user_name)
+        df_bookings['room_id'] = df_bookings['room_id'].map(to_room_name)
+        df_bookings['start_datetime'] = \
+            df_bookings['start_datetime'].map(to_datetime)
+        df_bookings['end_datetime'] = \
+            df_bookings['end_datetime'].map(to_datetime)
+
+        df_bookings = df_bookings.rename(columns={
+            'user_id': '予約者名',
+            'room_id': '会議室名',
+            'num_people': '予約人数',
+            'start_datetime': '開始時刻',
+            'end_datetime': '終了時刻',
+            'booking_id': '予約番号'
+        })
+
+        st.table(df_bookings)
+    else:
+        st.write('現在予約はありません')
 
     with st.form(key=page):
         user_name = st.selectbox('予約者名', users_name.keys())
