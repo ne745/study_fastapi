5409761226c7b3d6b4938a252e620e2f37276282
ne745 <kkpma0@gmail.com>
2022-09-20 21:34:42 +0900

    予約一覧をメインページに表示するように変更

--- reservation_system/pages/booking.py
+++ reservation_system/pages/booking.py
@@ -7,7 +7,7 @@ import requests
 import streamlit as st
 
 from pages.room import generate_rooms_name, read_room
-from pages.user import generate_user_name, read_user
+from pages.user import generate_user_name
 
 
 config = configparser.ConfigParser()
@@ -30,9 +30,6 @@ def create_booking():
     users_name = generate_user_name()
     rooms_name = generate_rooms_name()
 
-    users = read_user()
-    rooms = read_room()
-
     if not users_name:
         st.error('ユーザを登録してください')
     if not rooms_name:
@@ -42,52 +39,10 @@ def create_booking():
         return
 
     st.write('### 会議室一覧')
+    rooms = read_room()
     df_rooms = pd.DataFrame(rooms)
     df_rooms.columns = ['会議室名', '定員', '会議室 ID']
     st.table(df_rooms)
-
-    st.write('### 予約一覧')
-    bookings = read_booking()
-    df_bookings = pd.DataFrame(bookings)
-    if bookings:
-        # 予約があれば表示する
-        users_id = {}
-        for user in users:
-            users_id[user['user_id']] = user['user_name']
-
-        rooms_id = {}
-        for room in rooms:
-            rooms_id[room['room_id']] = {
-                'room_name': room['room_name'],
-                'capacity': room['capacity'],
-            }
-
-        # ID -> 値に変更
-        to_user_name = lambda x: users_id[x]  # noqa: E731
-        to_room_name = lambda x: rooms_id[x]['room_name']  # noqa: E731
-        to_datetime = lambda x: \
-            datetime.datetime.fromisoformat(x) \
-            .strftime('%Y/%m/%d %H:%M')  # noqa: E731
-
-        df_bookings['user_id'] = df_bookings['user_id'].map(to_user_name)
-        df_bookings['room_id'] = df_bookings['room_id'].map(to_room_name)
-        df_bookings['start_datetime'] = \
-            df_bookings['start_datetime'].map(to_datetime)
-        df_bookings['end_datetime'] = \
-            df_bookings['end_datetime'].map(to_datetime)
-
-        df_bookings_ja = df_bookings.rename(columns={
-            'user_id': '予約者名',
-            'room_id': '会議室名',
-            'num_people': '予約人数',
-            'start_datetime': '開始時刻',
-            'end_datetime': '終了時刻',
-            'booking_id': '予約番号'
-        })
-
-        st.table(df_bookings_ja)
-    else:
-        st.write('現在予約はありません')
 
     st.write('## 予約')
     with st.form(key=PAGE):
